7abaee3e68eacc0eb8b55c5b93a0c4eb86032fdc
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-20 11:01:16 +0300

    Migrate and Seed DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :todos
+  # resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   scope '/api/v1' do
-    resources :todolist
+    resources :todos
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Todo.create(title: "Watch movie", completed: false)
+Todo.create(title: "Wash dishes", completed: false)
