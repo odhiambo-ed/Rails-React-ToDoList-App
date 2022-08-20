1c6ee42975f4684ee639669cfde984df5f407a5b
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-20 12:29:35 +0300

    Fetch Todo Items

--- todo-app/src/components/TodoContainer.js
+++ todo-app/src/components/TodoContainer.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-class TdlistsContainer extends Component {
+class TodoContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [],
+        };
+    }
+
+    loadtodo() {
+        axios
+            .get("/api/v1/todos")
+            .then((res) => {
+                this.setState({ todos: res.data });
+            })
+            .catch((error) => console.log(error));
+    }
+
+    componentDidMount() {
+        this.loadtodo();
+    }
+
     render() {
         return (
             <div>
@@ -10,14 +31,25 @@ class TdlistsContainer extends Component {
                         type="text"
                         placeholder="Input a New Task and Press Enter"
                         maxLength="75"
+                        onKeyPress={this.createTodo}
                     />
                 </div>
                 <div className="wrapItems">
-                    <ul className="listItems"></ul>
+                    <ul className="listItems">
+                        {this.state.todos.map((tdlist) => {
+                            return (
+                                <li className="item" tdlist={tdlist} key={tdlist.id}>
+                                    <input className="itemCheckbox" type="checkbox" />
+                                    <label className="itemDisplay">{tdlist.title}</label>
+                                    <span className="removeItemButton">x</span>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
         );
     }
 }
 
-export default TdlistsContainer;
+export default TodoContainer;
